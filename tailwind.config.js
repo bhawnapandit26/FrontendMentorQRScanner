333a903d41dfccdf27c766afa4dcd54bf8454491
Bhawna Pandit <142803539+bhawnapandit26@users.noreply.github.com>
2023-08-25 22:37:08 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {},
+    colors:{
+      'gray-blue':'#7b879d',
+      'white': '#ffffff' ,
+      'light-gray': '#d6e2f0',
+      'dark-blue':'#1f3251' ,
+    },
+  
+  },
+  plugins: [],
+}
+
